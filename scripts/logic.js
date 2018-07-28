c250f5d9d8e6e02b1e50c23dc48d2de5cd771917
George <danchev49@protonmail.com>
2018-07-28 13:01:53 +0100

    mobile menu implemented.

--- scripts/logic.js
+++ scripts/logic.js
@@ -14,3 +14,27 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+var htmlWrapper = document.querySelector("body");
+
+htmlWrapper.addEventListener("click", (event) => {
+  if (event.target.tagName == "BUTTON") {
+    if (event.target.id == "menuBtnDiv") {
+      menuBtnFunc();
+    }
+  }
+});
+
+
+function menuBtnFunc() {
+  var visState = document.getElementById("mainNav");
+
+  if (visState.style.display === "none")
+  {
+    visState.style.display = "block";
+  }
+  else {
+    visState.style.display = "none";
+  }
+}
