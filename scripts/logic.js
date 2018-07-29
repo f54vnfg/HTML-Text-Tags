92a43724db4b5b7fb0d24a19034829bc4b2ac485
George <danchev49@protonmail.com>
2018-07-29 08:01:14 +0100

    Small tweaks

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,23 +1,31 @@
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+var htmlWrapper = document.querySelector("body");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction()
+};
+
+//
+window.onload = function() {
+  menuBtnFunc();
+}
+
+//
 function scrollFunction() {
-    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-        document.getElementById("bckBtn").style.display = "block";
-    } else {
-        document.getElementById("bckBtn").style.display = "none";
-    }
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    document.getElementById("bckBtn").style.display = "block";
+  } else {
+    document.getElementById("bckBtn").style.display = "none";
+  }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-
-var htmlWrapper = document.querySelector("body");
-
+//
 htmlWrapper.addEventListener("click", (event) => {
   if (event.target.tagName == "BUTTON") {
     if (event.target.id == "menuBtnDiv") {
@@ -26,17 +34,15 @@ htmlWrapper.addEventListener("click", (event) => {
   }
 });
 
-
+//
 function menuBtnFunc() {
   var visState = document.getElementById("mainNav");
   var mainHeading = document.getElementById("mainHeading");
 
-  if (visState.style.display === "none")
-  {
+  if (visState.style.display === "none") {
     visState.style.display = "block";
     mainHeading.style.display = "none";
-  }
-  else {
+  } else {
     visState.style.display = "none";
     mainHeading.style.display = "block";
   }
