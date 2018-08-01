443d1bdc2d2a9c643a9ba71a5273831a0f11bb1a
f54vnfg <danchev49@protonmail.com>
2018-08-01 20:58:19 +0100

    I removed flexbox from the nav menu and added code a bunch of comments.

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,16 +1,12 @@
 var htmlWrapper = document.querySelector("body");
 var navWrapper = document.querySelector(".navWrapper");
 
-// When the user scrolls down 500px from the top of the document, show the button
+// On scroll call this function
 window.onscroll = function() {
   scrollFunction()
 };
 
-//
-// window.onload = function() {
-// }
-
-//
+// When the user scrolls down 500px, show the button
 function scrollFunction() {
   if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
     document.getElementById("bckBtn").style.display = "block";
@@ -19,7 +15,7 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// When the user clicks on the button, scroll to the top
 function topFunction() {
   // For Safari
   document.body.scrollTop = 0;
@@ -27,24 +23,32 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
+// Add click event listener to the html tag
 htmlWrapper.addEventListener("click", (event) => {
+  // when you click on a tag
   if (event.target.tagName == "A") {
+    // are we clicking within the nav id
     if (event.target.parentNode.parentNode.parentNode.id == "mainNav") {
-      toggleMonileMenu(false);
-    }
+      // toggle mobile menu visability.
+      toggleMonileMenu();
+    } // check if we are clicking the mobile menu button
+  }  else if (event.target.id == "mobBtnDiv" || event.target.id == "menuBtnDiv") {
+    // toggle mobile menu visability.
+    toggleMonileMenu();
   }
 });
 
-document.getElementById('mobBtnDiv').onclick = function() {
-  toggleMonileMenu();
-}
-
+// toggle mobile nav menu
 function toggleMonileMenu() {
+  // variable to hold the classname
   var className = '' + navWrapper.className + '';
+  // the rest should be self explanatory
   if (~className.indexOf(' inactive ')) {
     navWrapper.className = className.replace(' inactive', ' active');
+    //
   } else if (~className.indexOf(' active ')) {
     navWrapper.className = className.replace(' active', ' inactive');
+    //
   } else {
     navWrapper.className += ' active ';
   }
