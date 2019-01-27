8adebd317e00fbffc36d9687f874ff22e6d28635
Asda Asdasd <sadfasdf@gmail.com>
2019-01-27 19:26:38 +0000

    mobile menu animation

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,15 +1,18 @@
-
 const menu_btn = document.querySelector('.menu-btn');
 const mainNav = document.querySelector('.mainNav__wrap');
 
-if(menu_btn !=null & mainNav !=null) {
-  menu_btn.addEventListener('click', function(e) {
-    if(mainNav.dataset.active == "false") {
+if (menu_btn != null & mainNav != null) {
+  menu_btn.addEventListener('click', function (e) {
+
+    menu_btn.classList.toggle('active');
+
+    if (mainNav.dataset.active == "false") {
       mainNav.classList.add('mainNav__wrap--active');
       mainNav.dataset.active = "true";
     } else {
       mainNav.classList.remove('mainNav__wrap--active');
       mainNav.dataset.active = "false";
     }
+
   });
 }
