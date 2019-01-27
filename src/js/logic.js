a17de298afaf9644375737d501828d27f5a042eb
Asda Asdasd <sadfasdf@gmail.com>
2019-01-27 20:38:17 +0000

    smooth scrolling logic - progress

---
+++ src/js/logic.js
@@ -0,0 +1,14 @@
+const navItems = document.querySelectorAll('.mainNav__item');
+
+if (navItems[0] != null) {
+  for (let i = 0; i < navItems.length; i++) {
+    navItems[i].addEventListener('click', function (e) {
+      const address = this.dataset.link;
+      // console.log(document.querySelector(`${address}`));
+      document.querySelector(`${address}`).scrollIntoView({
+        block: 'end',
+        behavior: 'smooth'
+      });
+    });
+  }
+}
